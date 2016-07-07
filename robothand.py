bf8623a22d61be15d6de4276d3b6278905433b78
maimuzo <maimuzo@gmail.com>
2016-07-08 02:00:12 +0900

    Rasberry Pi2/3のオンボードPWMで動作確認したが、チャネルが2つしかないので方針転換

--- robothand.py
+++ robothand.py
@@ -9,26 +9,80 @@ import time
 from pygame.locals import *
 import wiringpi
 
+class SG90:
+	# Raspberry Pi setting
+	PRI_PWM_BASE_CLOCK_FREQUENCY = 19200000 # 19.2MHz
+
+	# reference to: http://akizukidenshi.com/download/ds/towerpro/SG90_a.pdf
+	REFERENCE_PWM_1CYCLE_SEC = 0.02
+	MIN_ANGLE_SEC = 0.0005
+	MAX_ANGLE_SEC = 0.0024
+	MIN_ANGLE = -90
+	MAX_ANGLE = 90
+	MIN_PWM_VALUE = 0
+
+	def __init__(self, pin, pwmCountUpFrequency, pwmCycleRange):
+		# このservoが接続されるGPIO番号
+		self.pin = pin
+		self.pwmCountUpFrequency = pwmCountUpFrequency
+		self.pwmCycleRange = pwmCycleRange
+
+		# ピン設定
+		wiringpi.pinMode(pin, wiringpi.PWM_OUTPUT)
+
+		# see: http://qiita.com/locatw/items/f15fd9df40153bbb4d27
+		# PWMのカウンタ周期：19.2[MHz]/clock=19.2∗10^6/400=48[KHz]≒0.0208[ms]
+		self.pwmFrequency = float(SG90.PRI_PWM_BASE_CLOCK_FREQUENCY / self.pwmCountUpFrequency)
+		# PWM周波数：19.2[MHz]/clock/range=19.2∗106/400/1024=46.875[Hz]≒21.3[ms]
+		self.pwmCycleSec = 1.0 / self.pwmFrequency * self.pwmCycleRange
+		# 48[KHz]∗0.5[ms]=48000[Hz]∗0.0005[s]=24
+		self.minPmwValue = int(self.pwmFrequency * SG90.MIN_ANGLE_SEC)
+		# 48[KHz]∗2.4[ms]=48000[Hz]∗0.0024[s]=115.2≒115
+		self.maxPmwValue = int(self.pwmFrequency * SG90.MAX_ANGLE_SEC)
+		self.deltaPmwValue = self.maxPmwValue - self.minPmwValue
+
+		print 'pin: ' + str(self.pin) + ', pwmCountUpFrequency: ' + str(self.pwmCountUpFrequency) + 'Hz, pwmCycleRange: ' + str(self.pwmCycleRange)
+		print 'pwmFrequency: ' + ('%.3f' % self.pwmFrequency) + ' Hz(' + ('%.6f' % (1.0 / self.pwmFrequency)) + ' sec), pwmCycleSec: ' + ('%.6f' % self.pwmCycleSec)
+		print 'reference pwmCycleSec: ' + str(SG90.REFERENCE_PWM_1CYCLE_SEC)
+		print 'minPmwValue: ' + str(self.minPmwValue) + ', maxPmwValue: ' + str(self.maxPmwValue) + ', deltaPmwValue: ' + str(self.deltaPmwValue)
+
+	# 操作は角度(degree)で行う。中間地点を0度とし、MIN=-90度、MAX=90度とする
+	# PWMの操作は0〜pwmCycleRangeで行うので、このマッピングを行う
+	def _getPWMValue(self, degree):
+		if SG90.MIN_ANGLE > degree:
+			return SG90.MIN_PWM_VALUE
+		elif SG90.MAX_ANGLE < degree:
+			return self.pwmCycleRange - 1
+		else:
+			# マッピングする
+			# 0〜180度換算して%算出
+			percent = float(degree + 90) / 180
+			return self.minPmwValue + int(percent * self.deltaPmwValue)
+
+	def rotateTo(self, degree):
+		value = self._getPWMValue(degree)
+		wiringpi.pwmWrite(self.pin, value)
+		print 'pin: ' + str(self.pin) + ', degree: ' + ('%.6f' % degree) + ', pwmValue: ' + str(value)
+		time.sleep(0.3)
+
+
 class HandServo:
-	# pin mode
-	INPUT_MODE = 0
-	OUTPUT_MODE = 1
-	PWM_MODE = 2
 
 	MIN_VALUE = 0
 	MAX_VALUE = 1
-
 	INITIAL_SET = 0
+
+	PWM_COUNTUP_FREQUENCY = 400 # Hz
+	PWM_CYCLE_RANGE = 1024 # PWMの1サイクルの解像度0〜1023
 
 	# ServoをPWM制御するピン番号
 	# hardware pwm pin, see: http://abyz.co.uk/rpi/pigpio/python.html#hardware_PWM
-	# でも、software pwmならどのGPIOピンでも使える(?)
 	# $ gpio readall
 	# してBCMの列の数値を使う(wiringPiSetupGpio()しているので)
-	WAIST_SERVO_PIN = 2
-	BOOM_SERVO_PIN = 3
-	ARM_SERVO_PIN = 4
-	CRAW_SERVO_PIN = 14
+	WAIST_SERVO_PIN = 12
+	BOOM_SERVO_PIN = 13
+	ARM_SERVO_PIN = 18
+	CRAW_SERVO_PIN = 19
 
 	# 操作対象用インデックス
 	WAIST = 0 # 左右に回転する部分
@@ -37,59 +91,60 @@ class HandServo:
 	CRAW = 3 # バケットに相当する部分。先端の爪
 
 	# 操作対象とピン番号のマップ
-	PIN_DIC = {
+	PIN_MAP = {
 		0: WAIST_SERVO_PIN,
 		1: BOOM_SERVO_PIN,
 		2: ARM_SERVO_PIN,
 		3: CRAW_SERVO_PIN,
 	}
 
-	# 物理的な可動範囲[min, max] 0〜100の範囲内であること
-	# サンプルを参考に0〜180度を、0〜100に変換
+	# 物理的な可動範囲[min, max] -90〜90の範囲内であること
+	# サンプルを参考に0〜180度を、-90〜90度に変換
 	# see: https://www.mearm.com/blogs/news/74739717-mearm-on-the-raspberry-pi-work-in-progress
 	RANGE = (
-		(0, 100), # waist
-		(33, 92), # boom
-		(22, 100), # arm
-		(33, 100), # craw
+		(-90, 90), # waist
+		(-30, 75), # boom
+		(-50, 90), # arm
+		(-30, 90), # craw
 	)
 
 	# コントローラーの感度係数
 	SENSITIVITY = (
-		50, # waist
-		50, # boom
-		50, # arm
-		50, # craw
+		50.0, # waist
+		50.0, # boom
+		-50.0, # arm
+		50.0, # craw
 	)
 
-	# 定義済み位置(WAIST, BOOM, ARM, CRAW)
+	# 定義済み位置(WAIST, BOOM, ARM, CRAW) -90〜90の範囲
 	PRESET = (
-		(50, 85, 50, 33), # initial position
+		(0, 62, 0, -30), # initial position
 		(20, 30, 40, 50), # topキー用
 	)
 
 	def __init__(self):
 		# initial servo position
-		self.position = list(HandServo.PRESET[HandServo.INITIAL_SET]);
+		self.position = list(HandServo.PRESET[HandServo.INITIAL_SET])
 
 		# GPIO番号でピンを指定
 		wiringpi.wiringPiSetupGpio()
 
-		for index in HandServo.PIN_DIC.iterkeys():
-			pin = HandServo.PIN_DIC[index]
-			# ピンモード設定
-			wiringpi.pinMode(pin, HandServo.OUTPUT_MODE)
-			# 可動範囲設定 0〜100で指定
-			wiringpi.softPwmCreate(pin, 0, 100)
+		self.servos = []
+		for index in HandServo.PIN_MAP.iterkeys():
+			pin = HandServo.PIN_MAP[index]
+			# サーボを作る
+			self.servos.append(SG90(pin, HandServo.PWM_COUNTUP_FREQUENCY, HandServo.PWM_CYCLE_RANGE))
 
-			# wiringpi.pinMode(pin, wiringpi)
+		wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS)
+		wiringpi.pwmSetClock(HandServo.PWM_COUNTUP_FREQUENCY)
+		wiringpi.pwmSetRange(HandServo.PWM_CYCLE_RANGE)
 
 		# 初期位置設定
 		self._apply()
 
 
 	def update(self, powerList, delay):
-		for index in HandServo.PIN_DIC.iterkeys():
+		for index in HandServo.PIN_MAP.iterkeys():
 			self.position[index] += powerList[index] * delay * HandServo.SENSITIVITY[index]
 			if self.position[index] < HandServo.RANGE[index][HandServo.MIN_VALUE]:
 				self.position[index] = HandServo.RANGE[index][HandServo.MIN_VALUE]
@@ -103,12 +158,11 @@ class HandServo:
 
 	def _apply(self):
 		print '-------------'
-		for index in HandServo.PIN_DIC.iterkeys():
-			pin = HandServo.PIN_DIC[index]
-			# 端数を落として使う
-			value = int(self.position[index])
-			wiringpi.softPwmWrite(pin, value)
-			print self.getPartName(index) + ': ' + str(value)
+		for index in HandServo.PIN_MAP.iterkeys():
+			pin = HandServo.PIN_MAP[index]
+			degree = self.position[index]
+			print 'pin: ' + str(pin) + ' (' + self.getPartName(index) + '): ' + str(degree)
+			self.servos[index].rotateTo(degree)
 
 	def getPartName(self, index):
 		if(0 == index):
@@ -218,6 +272,7 @@ def main():
 				# よってバケットをクローに置き換えて
 				# (WAIST, ARM, BOOM, CRAW)の順に整理する
 				hand.update((x1, y1, y2, x2), delay)
+
 
 			elif e.type == pygame.locals.JOYBUTTONDOWN:
 				if pad.isPressed(PS3Pad.START):
