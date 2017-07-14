b52326f55679eba2a3cbad97c022623e557ee35e
maimuzo <maimuzo@gmail.com>
2017-07-14 12:21:44 +0900

    物理スイッチ長押し時に内蔵LED点滅

--- hard_switch.py
+++ hard_switch.py
@@ -10,9 +10,12 @@ import os
 
 watch_pin = 17
 wait_sec = 5
+internal_green_led_pin = 16
 
 GPIO.setmode(GPIO.BCM)
 
+# LEDコントロール用に16pinを出力モードに
+GPIO.setup(16, GPIO.OUT)
 
 # GPIO18pinを入力モードとし、pull up設定とします
 GPIO.setup(watch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -21,11 +24,19 @@ while True:
     # ボタンが押されるまで待つ
     GPIO.wait_for_edge(watch_pin, GPIO.FALLING)
     sw_counter = 0
+    led_is_on = False
 
     while True:
         sw_status = GPIO.input(watch_pin)
         if sw_status == 0:
             sw_counter = sw_counter + 1
+            if led_is_on:
+                # LED On
+                GPIO.output(16, GPIO.LOW)
+            else:
+                # LED Off
+                GPIO.output(16, GPIO.HIGH)
+            led_is_on = not led_is_on
             if sw_counter >= wait_sec:
                 print("長押し検知-shutdown")
                 os.system("sudo shutdown -h now")
@@ -38,3 +49,5 @@ while True:
         time.sleep(1)
 
     print(sw_counter)
+    # LED Off
+    GPIO.output(16, GPIO.HIGH)
